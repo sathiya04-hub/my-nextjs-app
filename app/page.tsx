44059247edf51bcf7e49f32b18326e9df290d56f
unknown <sathiya04@gmail.com>
2026-03-18 15:26:55 +0800

    fix: add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,50 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 import Chatbot from "@/components/Chatbot";
 import AIChatbot from "@/components/AIChatbot";
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div className="container">  
       <Image src="/banner1.jpg" alt="banner1" width={1200} height={800} style={{ width: "100%", height: "auto" }} />
-      <div className="navbar">
-        <div className="navbar-collapse">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link href="/pagination">
-                  Pagination
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/calc">
-                  Calculation
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/scroll">
-                  Scroll
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/upload">
-                  Upload
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/search">
-                  Search
-                </Link>   
-              </li>
-              <li className="nav-item">
-                <Link href="/login">
-                  Login
-                </Link>
-              </li>
-          </ul>
-        </div>
-        <Chatbot />
-        <AIChatbot />
-      </div>
+      <Link href="/upload">
+        Upload Page
+      </Link>
+      <Chatbot />
+      <AIChatbot />
     </div>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <div className="navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
                       <a className="nav-link" href="/pagination">Pagination</a>
