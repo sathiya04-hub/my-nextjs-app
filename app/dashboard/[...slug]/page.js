41f511f6a5abb99e11e937b5e413f580dc40cb22
unknown <sathiya04@gmail.com>
2026-03-25 13:53:14 +0800

    deployment fix

--- app/dashboard/[...slug]/page.js
+++ app/dashboard/[...slug]/page.js
@@ -1,17 +1,18 @@
 import { Suspense } from 'react'
+export const dynamic = "force-dynamic"; 
 
 export default async function Page({ params }) {
-  const { slug } = await params;
+  const { slug } = params;
 
   return (
-    <Suspense fallback={<div>Loading ...</div>}>
-    <div className="container">
-        <div className="box shadow p-4 mt-3">
-            <h1>Dashboard Page</h1>
-            <h3>Dynamic Page</h3>
-            <h4>Page Name: {slug.join(" / ")}</h4>
-        </div>
-    </div>
+    <Suspense fallback={<div>Loading dashboard...</div>}>
+      <div className="container">
+          <div className="box shadow p-4 mt-3">
+              <h1>Dashboard Page</h1>
+              <h3>Dynamic Page</h3>
+              <h4>Page Name: {slug.join(" / ")}</h4>
+          </div>
+      </div>
     </Suspense>
 
 
