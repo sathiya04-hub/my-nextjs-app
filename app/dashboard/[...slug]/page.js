5ca79c10fe8829d37d764a742ae604e89161db70
unknown <sathiya04@gmail.com>
2026-03-25 13:38:18 +0800

    deployment error fix

--- app/dashboard/[...slug]/page.js
+++ app/dashboard/[...slug]/page.js
@@ -1,8 +1,10 @@
+import { Suspense } from 'react'
+
 export default async function Page({ params }) {
   const { slug } = await params;
 
   return (
-
+    <Suspense fallback={<div>Loading ...</div>}>
     <div className="container">
         <div className="box shadow p-4 mt-3">
             <h1>Dashboard Page</h1>
@@ -10,6 +12,8 @@ export default async function Page({ params }) {
             <h4>Page Name: {slug.join(" / ")}</h4>
         </div>
     </div>
+    </Suspense>
+
 
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,15 @@ import { Suspense } from 'react'
 export default function Home() {
   return (
     <div className="container">  
+        <Suspense fallback={<div>Loading ...</div>}>
           <Image src="/banner1.jpg" alt="banner1" width={1200} height={800} style={{ width: "100%", height: "auto" }} />        
           <Link href="/upload">
             Upload Page
           </Link>
           <Chatbot />
-          <AIChatbot />      
+          <AIChatbot />
+        </Suspense>
+      
     </div>
   );
 }
